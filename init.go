087b2f59524678aaeb359874c926a6c0e84131cb
larkris <larkris@icloud.com>
2026-08-14 14:08:46 +0800

    Add files via upload

---
+++ init.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	_ "github.com/Loyalsoldier/geoip/plugin/maxmind"
+	_ "github.com/Loyalsoldier/geoip/plugin/mihomo"
+	_ "github.com/Loyalsoldier/geoip/plugin/plaintext"
+	_ "github.com/Loyalsoldier/geoip/plugin/singbox"
+	_ "github.com/Loyalsoldier/geoip/plugin/special"
+	_ "github.com/Loyalsoldier/geoip/plugin/v2ray"
+)
